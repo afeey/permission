f6e999eff92d6ffd6593563fb7efc55e3bc336a2
afeey <afeey@126.com>
2015-09-18 17:52:13 +0800

    update

--- src/main/java/com/afeey/permission/shiro/FilterChainDefinitionSection.java
+++ src/main/java/com/afeey/permission/shiro/FilterChainDefinitionSection.java
@@ -1,6 +1,7 @@
 package com.afeey.permission.shiro;
 
 import java.text.MessageFormat;
+import java.util.Set;
 
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.config.Ini.Section;
@@ -14,11 +15,11 @@ import org.springframework.beans.factory.FactoryBean;
  * @author Administrator
  *
  */
-public class FilterChainDefinitionSection implements FactoryBean<Ini.Section> {
+public class FilterChainDefinitionSection implements FactoryBean<Section> {
 
 	private static final Logger log = LoggerFactory
 			.getLogger(FilterChainDefinitionSection.class);
-	
+
 	// @Autowired
 	// private ResourceDao resourceDao;
 
@@ -31,23 +32,38 @@ public class FilterChainDefinitionSection implements FactoryBean<Ini.Section> {
 
 	@Override
 	public Section getObject() throws Exception {
-		
-		//加载默认urls
-		Ini ini=new Ini();
+
+		// 加载默认urls
+		Ini ini = new Ini();
 		ini.load(filterChainDefinitions);
-		Ini.Section section = ini.getSection(Ini.DEFAULT_SECTION_NAME);
-		
-		//加载权限
-		section.put("/admin/permission",  MessageFormat.format(PREMISSION_STRING,"permission"));
-		section.put("/admin/resource",  MessageFormat.format(PREMISSION_STRING,"resource"));
-		section.put("/admin/role",  MessageFormat.format(PREMISSION_STRING,"role"));
-		section.put("/admin/user",  MessageFormat.format(PREMISSION_STRING,"user"));
-		section.put("/admin/region",  MessageFormat.format(PREMISSION_STRING,"region"));
-		
-		if(log.isDebugEnabled()){
+		Section section = ini.getSection(Ini.DEFAULT_SECTION_NAME);
+
+		// 加载权限
+		section.put("/admin/permission",
+				MessageFormat.format(PREMISSION_STRING, "permission"));
+		section.put("/admin/resource",
+				MessageFormat.format(PREMISSION_STRING, "resource"));
+		section.put("/admin/role",
+				MessageFormat.format(PREMISSION_STRING, "role"));
+		section.put("/admin/user",
+				MessageFormat.format(PREMISSION_STRING, "user"));
+		section.put("/admin/region",
+				MessageFormat.format(PREMISSION_STRING, "region"));
+
+		if (log.isDebugEnabled()) {
+			log.debug(MessageFormat.format(PREMISSION_STRING, "permission"));
+
 			log.debug("get permission");
+			
+			Set<String> keySet=section.keySet();
+			for(String key : keySet){
+				//log.debug("key: {}, value: {}",section.get(key),section.);   
+			}
+			for (int i=0;i<keySet.size();i++) {
+				
+			}
 		}
-		
+
 		return section;
 	}
 
